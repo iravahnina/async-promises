9a66888d8725eb1d08a94f70fe49c18343021758
Ira Vakhnina <pirania943@gmail.com>
2021-04-24 21:58:50 +0300

    done

---
+++ src/js/GameSavingLoader.js
@@ -0,0 +1,10 @@
+import read from './reader.js';
+import json from './parser.js';
+
+export default class GameSavingLoader {
+
+  static load() {
+    return read().then((data) => json(data));
+  }
+  
+}
